08d96c85de768fba014eeef1226cd37d1108fb61
Juan Pablo Moreno <jupmorenor@gmail.com>
2020-06-14 21:53:24 -0500

    Obtención de datos y generacion de listado

---
+++ app/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+  Text,
+} from 'react-native';
+import Artist from './components/Artist'
+
+const API_KEY = '829751643419a7128b7ada50de590067';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.getData();
+    this.state = {
+      data: {topartists: {artist: []}}
+    }
+  }
+  
+  async getData() {
+    await fetch('https://ws.audioscrobbler.com/2.0/', {
+      method: 'post',
+      body: 'method=geo.gettopartists&country=colombia&api_key='+API_KEY+'&format=json',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      })
+    })
+    .then((response) => {
+      return response.text();
+    }).then((json) => {
+      this.setState({data: JSON.parse(json)})
+      //console.warn(this.state.data);
+      return json;
+    }).catch((error) => {
+      alert("error: " + error);
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Artistas mas escuchados en Colombia</Text>
+              </View>
+              <View style={styles.container}>
+                {
+                  this.state.data.topartists.artist.map(
+                    (artist, index) => {
+                      return (
+                        <View key={index} style={styles.artistContainer}>
+                          <Artist data={artist}/>
+                        </View>
+                      );
+                    }
+                  )
+                }
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#AAA',
+  },
+  body: {
+    backgroundColor: '#FFF',
+  },
+  container: {
+    //flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    flexWrap: 'nowrap',
+    backgroundColor: '#DDD'
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+  },
+  artistContainer: {
+    marginTop: 32,
+    flex: 0.5,
+    alignItems: 'center',
+    backgroundColor: '#AAA',
+    width: '50%',
+    borderRadius: 15,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '800',
+    color: '#000',
+  },
+});
+
+export default App;
---
+++ app/components/Artist.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { 
+  Text,  
+  Linking, 
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+class Artist extends Component {
+  constructor(props) {
+    super(props)
+    this.artist = props.data;
+  }
+  
+  async openLink(url) {
+    await Linking.openURL(url);
+  }
+
+  render() {
+    return (
+      <>
+        <TouchableOpacity onPress={() => this.openLink(this.artist.url)}>
+          {
+            this.artist.image.map(
+              (image, index) => {
+                if (image.size == "large") {
+                  return (
+                    <Image 
+                      key={index} 
+                      source={{uri: image["#text"]}} 
+                      style={styles.imageStyle} 
+                    />
+                  );
+                }
+            })
+          }
+        </TouchableOpacity>
+        <Text style={styles.sectionTitle}>{this.artist.name}</Text>
+        <Text style={styles.sectionDescription}>Listeners: {this.artist.listeners}</Text>
+        <Text style={styles.sectionDescription}>Streamable: {this.artist.streamable == "0" ? "No" : "Si" }</Text>
+      </>      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#000',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#000',
+  },
+  imageStyle : {
+    width: 150, 
+    height: 150, 
+    borderRadius: 15, 
+    marginTop: 5
+  }
+})
+
+export default Artist;
